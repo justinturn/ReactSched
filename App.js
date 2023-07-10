4302884525d39d4518278359998abdbc99797bd2
justinturn <justinturn@gmail.com>
2023-07-10 09:13:45 -0500

    working version

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import React from 'react';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+ <Image
+        style={styles.tinyLogo}
+        source={{
+          uri: 'https://axhaircoolers.com/wp-content/uploads/2022/01/Air-Coolers-Logo-a1.18.05.jpg',
+        }}
+      />
     </View>
   );
 }
@@ -17,4 +22,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  tinyLogo: {
+    width: 200,
+    height: 50,
+  },
 });
+
